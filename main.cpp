fdddfe37b780caf7e73aea4d55b597c5e80380d6
stacia <staciagreen.1@mail.ru>
2022-10-08 18:35:24 +0300

    Plus() added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+int Plus(int a, int b);
+
 int main() {
     std::cout << "Hello, this is SimpCalc!" << std::endl;
     return 0;
+}
+
+int Plus(int a, int b) {
+    return a + b;
 }
