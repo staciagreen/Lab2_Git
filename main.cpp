a5474f3e4355fc24be99b72f4705789f43736f56
Stacia <staciagreen.1@mail.ru>
2022-12-05 14:59:34 +0300

    first

--- main.cpp
+++ main.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
 
-float Plus(float a, float b);
-
-int main() {
-    std::cout << "43";
-    std::cout << "hello";
-    std::cout <<"fghjk";
-    std::cout << "helloo";
-    std::cout << "helloo";
-    return 0;
-}
